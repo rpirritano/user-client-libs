cc41070be7c1f62b5cec3960301e73e46306366d
Robert Pirritano <rpirritano@gmail.com>
2020-06-02 14:04:40 -0700

    completed userview

---
+++ src/main/java/com/app/user/dto/UserView.java
@@ -0,0 +1,45 @@
+package com.app.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserView {
+
+    private long id;
+    private String forename;
+    private String surname;
+    private String organisation;
+    private List<String> notifications;
+    private long points;
+
+    public UserView(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getOrganisation() {
+        return organisation;
+    }
+
+    public List<String> getNotifications() {
+        return notifications;
+    }
+
+    public long getPoints() {
+        return points;
+    }
+}
