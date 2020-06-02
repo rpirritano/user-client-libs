d7d232be9d782d821cd02181d7729a0091a08685
Robert Pirritano <rpirritano@gmail.com>
2020-06-02 13:59:59 -0700

    first commit

---
+++ src/main/java/com/app/UserClientLibsApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserClientLibsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserClientLibsApplication.class, args);
+	}
+
+}
